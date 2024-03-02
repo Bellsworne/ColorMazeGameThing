1ff38f3128c4d9f46802d0155747957bd7d3032b
Spudnut2000 <chrisbell@hexstudios.co>
2024-03-01 22:29:28 -0600

    Added sprinting and a few stat variables. TODO: Stamina system

--- Color/player/scripts/player.gd
+++ Color/player/scripts/player.gd
@@ -1,13 +1,21 @@
 class_name Player extends CharacterBody3D
 
 @export var speed: float = 5.0
+@export var sprint_speed_modifier: float = 5.0
 @export var jump_velocity: float = 4.5
 @export var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var mouse_sensitivity: float = 0.001
 @export var camera: Camera3D
 
-var orbintory : Orbintory = Orbintory.new()
+var max_health: int = 100
+var current_health: int = max_health
+var max_stamina: float = 100
+var current_stamina: float = max_stamina
+var remaining_lives: int = 3
 
+var is_sprinting: bool = false
+
+var orbintory : Orbintory = Orbintory.new()
 var look_dir: Vector2
 
 func _ready() -> void:
@@ -28,19 +36,26 @@ func _unhandled_input(event : InputEvent) -> void:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		
 func handle_movement(delta) -> void:
+	is_sprinting = Input.is_action_pressed("sprint")
+	
 	var _velocity: Vector3 = velocity
 	if (!is_on_floor()):
 		_velocity.y -= gravity * delta
 		
 	if (Input.is_action_just_pressed("jump") and is_on_floor()):
 		_velocity.y = jump_velocity
+		
 	
 	var input_direction = (Input.get_vector("left", "right", "up", "down"))
 	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
 	
 	if (direction != Vector3.ZERO):
-		_velocity.x = direction.x * speed
-		_velocity.z = direction.z * speed
+		if (is_sprinting):
+			_velocity.x = direction.x * (speed + sprint_speed_modifier)
+			_velocity.z = direction.z * (speed + sprint_speed_modifier)
+		else:
+			_velocity.x = direction.x * speed
+			_velocity.z = direction.z * speed
 	else:
 		_velocity.x = move_toward(velocity.x, 0, speed)
 		_velocity.z = move_toward(velocity.z, 0, speed)
