f248e92b5388c4705d8cc094bd7ea47d1c421966
Spudnut2000 <chrisbell@hexstudios.co>
2024-03-01 23:08:09 -0600

    Added stamina

--- Color/player/scripts/player.gd
+++ Color/player/scripts/player.gd
@@ -6,14 +6,14 @@ class_name Player extends CharacterBody3D
 @export var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var mouse_sensitivity: float = 0.001
 @export var camera: Camera3D
-
-var max_health: int = 100
-var current_health: int = max_health
-var max_stamina: float = 100
-var current_stamina: float = max_stamina
-var remaining_lives: int = 3
+@export var max_health: int = 100
+@export var current_health: int = max_health
+@export var max_stamina: int = 100
+@export var current_stamina: int = max_stamina
+@export var remaining_lives: int = 3
 
 var is_sprinting: bool = false
+var can_sprint: bool = true
 
 var orbintory : Orbintory = Orbintory.new()
 var look_dir: Vector2
@@ -34,17 +34,16 @@ func _unhandled_input(event : InputEvent) -> void:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		
+
+
 func handle_movement(delta) -> void:
-	is_sprinting = Input.is_action_pressed("sprint")
-	
 	var _velocity: Vector3 = velocity
+	
 	if (!is_on_floor()):
 		_velocity.y -= gravity * delta
 		
 	if (Input.is_action_just_pressed("jump") and is_on_floor()):
 		_velocity.y = jump_velocity
-		
 	
 	var input_direction = (Input.get_vector("left", "right", "up", "down"))
 	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
@@ -61,7 +60,30 @@ func handle_movement(delta) -> void:
 		_velocity.z = move_toward(velocity.z, 0, speed)
 
 	velocity = _velocity
+	handle_sprinting()
 	move_and_slide()
+
+
+func handle_sprinting():
+	if (is_sprinting and current_stamina > 0):
+		get_tree().create_timer(0.5).timeout
+		current_stamina -= 1
+	elif (!is_sprinting and current_stamina < max_stamina):
+		get_tree().create_timer(0.5).timeout
+		current_stamina += 1
+	
+	if (current_stamina <= 1 or !is_on_floor()):
+		can_sprint = false
+		is_sprinting = false
+	elif (current_stamina >= 1 and is_on_floor()):
+		can_sprint = true
+	
+	if (Input.is_action_just_pressed("sprint") and can_sprint):
+		is_sprinting = true
+	elif (Input.is_action_just_released("sprint")):
+		is_sprinting = false
+			
+	clampf(current_stamina, 0, max_stamina)
 
 
 func handle_camera_rotation(event : InputEvent) -> void:
